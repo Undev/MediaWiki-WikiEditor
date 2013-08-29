078cb881cbc04d02131a9fa1afc433d329dd2b17
Denisov Denis <denisovdenis@me.com>
2013-08-29 20:55:48 +0400

    Disable parse =

--- WikiEditor.php
+++ WikiEditor.php
@@ -63,7 +63,7 @@ $wgHooks['GetPreferences'][] = 'WikiEditorHooks::getPreferences';
 $wgHooks['ResourceLoaderGetConfigVars'][] = 'WikiEditorHooks::resourceLoaderGetConfigVars';
 $wgHooks['MakeGlobalVariablesScript'][] = 'WikiEditorHooks::makeGlobalVariablesScript';
 $wgHooks['EditPageBeforeEditToolbar'][] = 'WikiEditorHooks::EditPageBeforeEditToolbar';
-$wgHooks['ParserBeforeInternalParse'][] = 'WikiEditorHooks::onParserBeforeInternalParse';
+//$wgHooks['ParserBeforeInternalParse'][] = 'WikiEditorHooks::onParserBeforeInternalParse';
 
 $wikiEditorTpl = array(
 	'localBasePath' => dirname( __FILE__ ) . '/modules',
